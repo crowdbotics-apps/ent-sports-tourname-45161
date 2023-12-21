4996afd6359dbd3300f767a1cf242084964ab50f
Crowdbotics <team@crowdbotics.com>
2023-12-21 19:39:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ent-sports-tourname-45161",
+  url: "https://ent-sports-tourname-45161.botics.co",
+  api: "https://ent-sports-tourname-45161.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
